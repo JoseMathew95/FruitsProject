eaa5ea286d0d54d7d2df7b99be3d64df84e3feb4
Jose.Mathew <jose.mathew@autodatacorp.org>
2020-04-25 20:18:47 -0400

    completed the fruits project to learn mongoose

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true})
 
-  const fruitSchema = mongoose.Schema({
-    name: String,
-    rating: Number,
-    review: String
-  });
+const fruitSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required:[true, "Ples specify the Fruit name."]
+  },
+  rating: {
+    type: Number,
+    min:1,
+    max:10
+  },
+  review: String
+});
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-  const Fruit = mongoose.model("Fruit", fruitSchema);
+const personSchema = mongoose.Schema({
+  name: String,
+  Age: Number,
+  favouriteFruit: fruitSchema
+});
+const Person = mongoose.model("Person", personSchema);
 
-  const Apples = new Fruit({
-    name: "Apples",
-    rating: 7,
-    review: "Not bad, also healthy."
-  });
 
-  const Oranges = new Fruit({
-    name: "Oranges",
-    rating: 8,
-    review: "I like it. "
+  const kiwis = new Fruit({
+    name: "Kiwi",
+    rating: 9,
+    review: "I do not remember the taste."
   });
+  //kiwis.save();
+
+  const person = new Person({
+    name: "Jose Mathew",
+    Age: 2053,
+    favouriteFruit: kiwis
+  });
+  //person.save();
+
+  Person.updateOne({name:"Jesus Christ"},{favouriteFruit:kiwis},function(err){
+    if(err){
+      console.log(err)
+    } else {
+      console.log("Sucessfully updated the record.");
+    }
+  })
+
+
+
+
 
   // Fruit.insertMany([Apples,Oranges], function(err){
   //   if(err)
@@ -29,26 +57,27 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, 
   // })
   //fruit.save();
 
+// Fruit.find(function(err, fruits){
+//   if(err){
+//     console.log(err);
+//   }else{
+//       mongoose.connection.close();
+//       fruits.forEach(function(fruit){
+//         console.log(fruit.name);
+//       })
+//     }
+//   });
 
-const personSchema = mongoose.Schema({
-  name: String,
-  Age: Number
-});
-
-const Person = mongoose.model("Person", personSchema);
-const person = new Person({
-  name: "Jesus Christ",
-  Age: 2053
-});
-//person.save();
-
-Fruit.find(function(err, fruits){
-  if(err){
-    console.log(err);
-  }else{
-      mongoose.connection.close();
-      fruits.forEach(function(fruit){
-        console.log(fruit.name);
-      })
-    }
-  });
+// Fruit.updateOne({_id:"5ea235405e3a4d4b90cf972d"},{name:"Orange"}, function(err){
+//     if(err){
+//       console.log(err)} else{
+//         console.log("succesfully Updated the Record.")
+//       }
+//   })
+//
+// Person.deleteOne({_id:"5ea4d05aad8f325108d7551a"}, function(err){
+//   if(err){
+//     console.log(err)} else{
+//       console.log("succesfully Deleted the Record.")
+//     }
+// })
